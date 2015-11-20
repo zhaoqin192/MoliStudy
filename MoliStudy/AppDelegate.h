b4fc07c085dc93dc4352972dfb00bba1f10b3d56
zhaoqin <zhaoqin192@gmail.com>
2015-11-20 22:27:10 +0800

    create project

---
+++ MoliStudy/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MoliStudy
+//
+//  Created by zhaoqin on 11/20/15.
+//  Copyright © 2015 MoliStudy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MoliStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoliStudy
+//
+//  Created by zhaoqin on 11/20/15.
+//  Copyright © 2015 MoliStudy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
