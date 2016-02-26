6742aecd320f75735504e0ab4d37186b9b059d95
wangxiao <250455694@qq.com>
2016-02-26 23:46:03 +0800

    完成答题卡页面

---
+++ MoliStudy/MLQuestionCardCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLQuestionCardCollectionViewController.h
+//  MoliStudy
+//
+//  Created by 王霄 on 16/2/26.
+//  Copyright © 2016年 MoliStudy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLQuestionCardCollectionViewController : UICollectionViewController
+
+@end
---
+++ MoliStudy/MLQuestionCardCollectionViewController.m
@@ -0,0 +1,95 @@
+//
+//  MLQuestionCardCollectionViewController.m
+//  MoliStudy
+//
+//  Created by 王霄 on 16/2/26.
+//  Copyright © 2016年 MoliStudy. All rights reserved.
+//
+
+#import "MLQuestionCardCollectionViewController.h"
+
+@interface MLQuestionCardCollectionViewController ()
+
+@end
+
+@implementation MLQuestionCardCollectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"答题卡";
+    UICollectionViewFlowLayout *flowLayout=[[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    self.view = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, kScreen_Width, kScreen_Height) collectionViewLayout:flowLayout];
+    //设置代理
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    [self.view addSubview:self.collectionView];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+}
+
+#pragma mark -- UICollectionViewDataSource
+//定义展示的UICollectionViewCell的个数
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+//定义展示的Section的个数
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+//每个UICollectionView展示的内容
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identify = @"cell";
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identify forIndexPath:indexPath];
+   // [cell sizeToFit];
+    if (!cell) {
+        NSLog(@"无法创建CollectionViewCell时打印，自定义的cell就不可能进来了。");
+    }
+    //cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+//头部显示的内容
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:
+                                            UICollectionElementKindSectionHeader withReuseIdentifier:@"ReusableView" forIndexPath:indexPath];
+    
+    // [headerView addSubview:_headerView];//头部广告栏
+    return headerView;
+}
+#pragma mark --UICollectionViewDelegateFlowLayout
+//定义每个UICollectionView 的大小
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    //边距占5*4=20 ，2个
+    //图片为正方形，边长：(fDeviceWidth-20)/2-5-5 所以总高(fDeviceWidth-20)/2-5-5 +20+30+5+5 label高20 btn高30 边
+    //return CGSizeMake((fDeviceWidth-20)/2, (fDeviceWidth-20)/2+50);
+    return CGSizeMake((kScreen_Width - 80)/5, (kScreen_Width - 80)/5);
+}
+//定义每个UICollectionView 的间距
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(20, 20, 20, 20);
+}
+//定义每个UICollectionView 纵向的间距
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+#pragma mark --UICollectionViewDelegate
+//UICollectionView被选中时调用的方法
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    //    UICollectionViewCell * cell = (UICollectionViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
+    //    cell.backgroundColor = [UIColor redColor];
+    NSLog(@"选择%ld",indexPath.row);
+}
+//返回这个UICollectionView是否可以被选择
+-(BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+@end
